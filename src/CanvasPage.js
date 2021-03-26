22d6dfae6a28a0ee735908d93580e7e13f35dd3d
Andrew <apester@iastate.edu>
2021-03-26 12:42:34 -0500

    finished

--- src/CanvasPage.js
+++ src/CanvasPage.js
@@ -1,16 +1,13 @@
-import './CanvasPage.css';
-import { useEffect, useRef, useState } from 'react';
-import { ToolPanel } from './ToolPanel';
-import { PencilTool } from './pencilTool';
-import { EraserTool } from './eraserTool';
+import "./CanvasPage.css";
+import { useEffect, useRef, useState } from "react";
+import { ToolPanel } from "./ToolPanel";
+import { PencilTool } from "./pencilTool";
+import { EraserTool } from "./eraserTool";
 
 /**
  * An instance of each tool
  */
- const tools = [
-	new PencilTool(),
-	new EraserTool()
-]
+const tools = [new PencilTool(), new EraserTool()];
 
 const CANVAS_WIDTH = 600;
 const CANVAS_HEIGHT = 400;
@@ -19,48 +16,77 @@ const CANVAS_HEIGHT = 400;
  * Canvas Page
  */
 export function CanvasPage() {
-	const [currentTool, setCurrentTool] = useState(tools[0]);
-	const [mouseDown, setMouseDown] = useState(false);
-	const [color] = useState('black');
-	const canvasRef = useRef();
-	const ctx =  canvasRef.current?.getContext('2d');
+  const [currentTool, setCurrentTool] = useState(tools[0]);
+  const [mouseDown, setMouseDown] = useState(false);
+  const [color] = useState("black");
+  const canvasRef = useRef();
+  const ctx = canvasRef.current?.getContext("2d");
 
-	useEffect(() => {
-		if (!!ctx) {
-			ctx.fill = color;
-		}
-	}, [ctx, color]);
+  useEffect(() => {
+    if (!!ctx) {
+      ctx.fill = color;
+    }
+  }, [ctx, color]);
 
-	function onMouseUp(e) {
-		setMouseDown(false);
-		currentTool.onMouseUp({x: e.pageX - 200, y: e.pageY}, ctx);
-	}
+  function onMouseUp(e) {
+    setMouseDown(false);
+    currentTool.onMouseUp({ x: e.pageX - 200, y: e.pageY }, ctx);
+  }
 
-	function onMouseDown(e) {
-		setMouseDown(true);
-		currentTool.onMouseDown({x: e.pageX - 200, y: e.pageY}, ctx);
-	}
+  function onMouseDown(e) {
+    setMouseDown(true);
+    currentTool.onMouseDown({ x: e.pageX - 200, y: e.pageY }, ctx);
+  }
 
-	function onMouseMove(e) {
-		if (mouseDown) {
-			currentTool.onMouseMove({x: e.pageX - 200, y: e.pageY}, ctx);
-		}
-	}
+  function onMouseMove(e) {
+    if (mouseDown) {
+      currentTool.onMouseMove({ x: e.pageX - 200, y: e.pageY }, ctx);
+    }
+  }
+  function handleImage(e) {
+    var img = new Image();
+    img.onload = draw;
+    img.src = URL.createObjectURL(e.target.files[0]);
+  }
+  function draw() {
+    var ctx = canvasRef.current.getContext("2d");
+    ctx.drawImage(this, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  }
+  function download(){
+	var canvas = document.getElementById('mainCanvas');
+	const image = canvas.toDataURL();
+	const link = document.createElement('a');
+	link.href = image;
+	link.download = 'image.png';
+	link.click();
+  }
 
-	return (
-		<div id="canvasPageContainer">
-			<ToolPanel
-				currentTool={currentTool}
-				toolList={tools}
-				setCurrentTool={setCurrentTool} />
-			<canvas
-				id="mainCanvas"
-				ref={canvasRef}
-				width={CANVAS_WIDTH}
-				height={CANVAS_HEIGHT}
-				onMouseUp={onMouseUp}
-				onMouseDown={onMouseDown}
-				onMouseMove={onMouseMove} />
-		</div>
-	)
+
+  return (
+    <div id="canvasPageContainer">
+      <ToolPanel
+        currentTool={currentTool}
+        toolList={tools}
+        setCurrentTool={setCurrentTool}
+      />
+      <canvas
+        id="mainCanvas"
+        ref={canvasRef}
+        width={CANVAS_WIDTH}
+        height={CANVAS_HEIGHT}
+        onMouseUp={onMouseUp}
+        onMouseDown={onMouseDown}
+        onMouseMove={onMouseMove}
+      />
+      <div>
+        <input
+          type="file"
+          id="file"
+          accept="image/*"
+          onChange={handleImage}
+        ></input>
+		<button onClick = {download}>Download</button>
+      </div>
+    </div>
+  );
 }
