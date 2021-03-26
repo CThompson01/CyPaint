8def124853f8bc0bff9c1a070a27b48db5733fd7
CThompson01 <CThompson10547@gmail.com>
2021-03-26 09:30:45 -0500

    Added Shape Functionality
    Added the ability to click on the canvas and create shapes such as squares, triangles, and circles.

---
+++ src/circleTool.js
@@ -0,0 +1,23 @@
+import circle from './circle.svg'
+import { Tool } from './tool'
+
+var originPoint = [-1,-1]
+
+export class CircleTool extends Tool {
+	icon = circle
+	name = 'Circle Tool'
+	id = 'tool.circle'
+
+    onMouseDown(mousePos, ctx) {
+        if (originPoint[0] === -1) {
+            originPoint = [mousePos.x, mousePos.y]
+        } else {
+            let radius = Math.sqrt(Math.pow(mousePos.x - originPoint[0], 2) + Math.pow(mousePos.y - originPoint[1], 2))
+            ctx.beginPath()
+            ctx.arc(originPoint[0], originPoint[1], radius, 0, 2*Math.PI)
+            // ctx.fill() broken for some reason check back later
+            ctx.stroke()
+            originPoint = [-1,-1]
+        }
+    }
+}
