f26e1f64537e0d73288c06b37e41a89709ea4c03
Andrew <apester@iastate.edu>
2021-04-23 23:28:08 -0500

    fixed issue with selecting black or white on canvas

--- src/tools/colorPickerTool.js
+++ src/tools/colorPickerTool.js
@@ -11,13 +11,20 @@ export class ColorPickerTool extends Tool {
         var p = ctx.getImageData(mousePos.x, mousePos.y, 1, 1).data;
         var r = p[0];
         var g = p[1];
-        var b = p[2]; 
+        var b = p[2];
+        var a = p[3]; 
+        console.log(p);
         if(r>255||g>255||b>255){
             console.log("invalid color");
         }else{
-            var hex = "#" + ("000000" +  ((r << 16) | (g << 8) | b).toString(16)).slice(-6);
-            //console.log(hex);
-            ctx.fillStyle = hex;
+            if(r==0 && g == 0 && b == 0 & a ==0){
+                //have to do this otherwise when clicking on transparent area it 
+                //returns transparant black which is unhelpful to draw with so set it to with, the color of the default background
+                r = g = b = a = 255;
+            }
+            const rgba = `rgba(${r}, ${g}, ${b}, ${a / 255})`;
+            console.log(rgba);
+            ctx.fillStyle = rgba;
         }
 	}
 }
